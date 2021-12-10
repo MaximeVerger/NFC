1dc6a39fa8dd1709352b1338250c1e0be5f49aa8
MaximeVerger <vergermaxime.sio@gmail.com>
2021-12-10 09:32:19 +0100

    permission6

--- nfc.js
+++ nfc.js
@@ -1,24 +1,26 @@
-const ndef = new NDEFReader();
+// const ndef = new NDEFReader();
 
-async function startScanning() {
-    await ndef.scan();
-    ndef.onreading = event => {
-    /* handle NDEF messages */
-    };
-}
+// async function startScanning() {
+//     await ndef.scan();
+//     ndef.onreading = event => {
+//     /* handle NDEF messages */
+//     };
+// }
 
 const nfcPermissionStatus = await navigator.permissions.query({ name: "nfc" });
-if (nfcPermissionStatus.state === "granted") {
-  // NFC access was previously granted, so we can start NFC scanning now.
-    startScanning();
-} else {
-  // Show a "scan" button.
-    document.querySelector("#scanButton").style.display = "block";
-    document.querySelector("#scanButton").onclick = event => {
-    // Prompt user to allow UA to send and receive info when they tap NFC devices.
-    startScanning();
-    };
-}
+consoleLog(nfcPermissionStatus)
+
+// if (nfcPermissionStatus.state === "granted") {
+//     // NFC access was previously granted, so we can start NFC scanning now.
+//     startScanning();
+// } else {
+//     // Show a "scan" button.
+//     document.querySelector("#scanButton").style.display = "block";
+//     document.querySelector("#scanButton").onclick = event => {
+//     // Prompt user to allow UA to send and receive info when they tap NFC devices.
+//     startScanning();
+//     };
+// }
 
 async function readTag() {
     if ("NDEFReader" in window) {
