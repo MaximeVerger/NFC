41b1a28fa15afc75154a4f7026475eba1cf87f02
MaximeVerger <vergermaxime.sio@gmail.com>
2021-12-10 10:00:20 +0100

    perm

--- nfc.js
+++ nfc.js
@@ -63,39 +63,56 @@
 //     logElement.innerHTML += data + '\n';
 // }
 
-function displayOutcome(type, outcome) {
-    return function() {
-        var argList = [outcome, type].concat([].slice.call(arguments));
-        switch(outcome) {
-            case "success":
-            console.info.apply(console, argList);
-            break;
-            case "error":
-            console.error.apply(console, argList);
-            break;
-            default:
-            console.log.apply(console, argList);
-        }
-        document.getElementById(type).classList.remove('success', 'error', 'default');
-        document.getElementById(type).classList.add(outcome);
+window.addEventListener("load", function() {
+
+    var toggle = document.querySelector("#toggle");
+    toggle.classList.add("instant");
+    if (window.location.protocol == "https:") {
+        toggle.classList.add("https");
+        toggle.protocol = "http:";
+    } else if (window.location.protocol == "http:") {
+        toggle.classList.add("http");
+        toggle.protocol = "https:";
+    }
+    setTimeout(function() {
+        toggle.classList.remove("instant");
+    }, 10);
+
+    function displayOutcome(type, outcome) {
+        return function() {
+            var argList = [outcome, type].concat([].slice.call(arguments));
+            switch(outcome) {
+                case "success":
+                console.info.apply(console, argList);
+                break;
+                case "error":
+                console.error.apply(console, argList);
+                break;
+                default:
+                console.log.apply(console, argList);
+            }
+            document.getElementById(type).classList.remove('success', 'error', 'default');
+            document.getElementById(type).classList.add(outcome);
+        };
     };
-};
 
-var register = {
-    "nfc": function() {
-        if ('NDEFReader' in window) {
-            const reader = new NDEFReader();
-            reader.scan()
-            .then(() => {
-                displayOutcome("nfc", "success")("Successfully started NFC scan");
-            })
-            .catch((err) => {
-                displayOutcome("nfc", "error")(err);
-            });
-        } else {
-            displayOutcome("nfc", "error")("NDEFReader is not available");
-        }
-    },
-}
+    var register = {
+        "nfc": function() {
+            if ('NDEFReader' in window) {
+                const reader = new NDEFReader();
+                reader.scan()
+                .then(() => {
+                    displayOutcome("nfc", "success")("Successfully started NFC scan");
+                })
+                .catch((err) => {
+                    displayOutcome("nfc", "error")(err);
+                });
+            } else {
+                displayOutcome("nfc", "error")("NDEFReader is not available");
+            }
+        },
+    }
 
-document.getElementById('nfc').addEventListener('click', register['nfc'])
+    document.getElementById('nfc').addEventListener('click', register['nfc'])
+
+});
